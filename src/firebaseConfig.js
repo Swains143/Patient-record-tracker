15788f8210e4b0b7aa89576951fd63901e6647b1
Swains143 <obayaro@proton.me>
2025-08-25 00:08:49 +0300

    configuring firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,13 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCjb-VeVi1aUKZgxqRXlvQZi_gxNLWO1rk",
   authDomain: "patientrecordtracker.firebaseapp.com",
   projectId: "patientrecordtracker",
-  storageBucket: "patientrecordtracker.firebasestorage.app",
+  storageBucket: "patientrecordtracker.appspot.com",
   messagingSenderId: "92767764847",
   appId: "1:92767764847:web:6bd4d8b23ff94bb93c289a"
 };
@@ -14,5 +15,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore
+// Export Firestore database
 export const db = getFirestore(app);
